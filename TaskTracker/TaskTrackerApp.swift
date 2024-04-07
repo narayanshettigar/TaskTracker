608708a475bdd553b5d6c8727c9c9300570057b7
narayanshettigar <narayanshettigar16@gmail.com>
2024-04-07 12:44:04 +0530

    Initial Commit

---
+++ TaskTracker/TaskTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTrackerApp.swift
+//  TaskTracker
+//
+//  Created by Narayan Shettigar on 07/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
